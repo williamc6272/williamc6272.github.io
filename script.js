c10e497428cc3557671c1365087e8866c0612fa9
williamc6272 <williamc522@nycstudents.net>
2025-09-30 09:37:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "William C"; // update the part BETWEEN the "quotes"
+let userName = "williamc6272.github.io"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
